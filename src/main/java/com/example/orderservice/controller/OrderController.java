8b10b31dc0b1adea3f76d27ec9b533496bfe6007
Test User <test@example.com>
2026-02-28 08:02:20 -0500

    final

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,8 +1,8 @@
 package com.example.orderservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
@@ -10,6 +10,6 @@ public class OrderController {
 
     @GetMapping("/createorder")
     public String getOrders() {
-        return "From the Order Service: Order created successfully!";
+        return "From the Order Service: Order created hjhjhjh!";
     }
 }
